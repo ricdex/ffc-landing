5b3048c251628cc01d7b2f0d126fb1cdea111c17
Ricardo Avila <ricdex@gmail.com>
2021-05-17 18:48:11 -0500

    first version

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: {
+   
+   content: ['./public/**/*.html'],
+},
+darkMode: false, // or 'media' or 'class'
+theme: {
+ extend: {
+   colors: {
+     'ffc1': '#00EEFF',
+     'ffc2': '#7C30FF',
+     'ffc1.5': '#4289FF',
+     
+   }
+ },
+},
+variants: {
+ extend: {},
+},
+plugins: [],
+}
